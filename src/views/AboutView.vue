7f9d9091b93cc81224171068d4f37ebe1f7b7846
sky <gokturk.acr2002@gmail.com>
2022-12-22 01:52:02 +0300

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <p v-for="user in users" :key="user.id">
+    {{user.name}}
+  </p>
+</template>
+
+<script>
+import userData from '../assets/users.json'
+
+export default {
+  data() {
+    return {
+      users: userData,
+    };
+  },
+};
+</script>
---
+++ src/views/videos.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+h6 Videos
+    p(v-for='video in videos' :key='video.url')
+        | {{video.title}}
+</template>
+  
+<script>
+  import VideoData from '../assets/videos.json'
+  
+  export default {
+    data() {
+      return {
+        videos: VideoData['videos'],
+      };
+    },
+  };
+  </script>

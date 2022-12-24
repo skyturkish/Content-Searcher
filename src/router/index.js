0f996996096ffacb8c84a5459cc59a19e25cd616
sky <gokturk.acr2002@gmail.com>
2022-12-24 16:12:07 +0300

    remove unnecessary routes

--- src/router/index.js
+++ src/router/index.js
@@ -9,22 +9,22 @@ const router = createRouter({
       name: "home",
       component: HomeView,
     },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
-    },
-    {
-      path: "/videos",
-      name: "videos",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/videos.vue"),
-    },
+    // {
+    //   path: "/about",
+    //   name: "about",
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import("../views/AboutView.vue"),
+    // },
+    // {
+    //   path: "/videos",
+    //   name: "videos",
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import("../views/videos.vue"),
+    // },
   ],
 });
 

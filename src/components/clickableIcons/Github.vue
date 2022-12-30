ec5973d037dc921fa560743fb64698a57477c0ed
sky <gokturk.acr2002@gmail.com>
2022-12-30 17:13:31 +0300

    clickable icon and components

---
+++ src/components/clickableIcons/Github.vue
@@ -0,0 +1,8 @@
+<script setup>
+import GithubIcon from "../images/githubIcon.png"    
+</script>
+
+<template lang="pug">
+a(href="https://github.com/skyturkish/content-searcher" target='_blank')
+  img(:src='GithubIcon' width="48" height="48")
+</template>
---
+++ src/components/clickableIcons/Youtube.vue
@@ -0,0 +1,9 @@
+<script setup>
+import YoutubeIcon from "../images/youtubeIcon.png"
+    
+</script>
+
+<template lang="pug">
+a(href="https://www.youtube.com/@ArmaganAmcalar" target='_blank')
+  img(:src='YoutubeIcon' width="48" height="48")
+</template>

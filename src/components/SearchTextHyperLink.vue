afac38f99e9a1580fad01253c6344383dc0cc8d0
sky <gokturk.acr2002@gmail.com>
2022-12-30 17:16:20 +0300

    leads to the relevant youtube vide It redirects to a youtube video exactly to the time stamp we want it to start

---
+++ src/components/SearchTextHyperLink.vue
@@ -0,0 +1,36 @@
+<script setup >
+const props = defineProps({
+  video : {
+    type: Map,
+    required: true
+  },
+  timeStamp: {
+    type: Map,
+    required: true
+  }
+})
+
+function startWith(timeStamp) {
+  const time = timeStamp.time.split(':')
+
+  const minute = time[0]
+
+  const second = time[1]
+  
+  const startSecond = parseInt(minute) * 60 + parseInt(second)
+  
+  return '&t=' + startSecond + 's'
+}
+
+</script>
+
+<template lang="pug">
+.link
+  a(:href='video.url + startWith(timeStamp)' target='_blank') {{ timeStamp.text }} {{ video.createdAt }} 
+</template>
+
+<style scoped> 
+.link{
+  margin-bottom:10px;
+}
+</style> 
